2eeacefbed001de732c24a210139687eae9b357e
Thomas Lincoln <thmas5952@gmail.com>
2025-08-02 13:06:41 -0300

    feat: :lipstick: Update InputField

--- frontend/my-invest/src/components/InputField/InputField.vue
+++ frontend/my-invest/src/components/InputField/InputField.vue
@@ -11,18 +11,14 @@ const model = defineModel<string | number>();
 </script>
 <template>
   <div class="flex flex-col mb-4">
-    <label :for="id" class="block text-gray-200 text-lg font-bold mb-2">
+    <label class="block text-gray-200 text-lg font-bold mb-2">
       {{ label }}
     </label>
     <input
       v-model="model"
       :type="type"
       :placeholder="placeholder"
-      :id="id"
-      class="outline-none border-b-2 border-b-indigo-500"
+      class="border-b-2 focus:border-violet-600 outline-hidden"
     />
   </div>
 </template>
-<style>
-@import "tailwindcss";
-</style>
