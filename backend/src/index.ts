65a63a3b002313838a38c5fea3e163992e26fd12
Thomas Lincoln <thmas5952@gmail.com>
2025-08-15 14:31:56 -0300

    feat: :sparkles: Add error handling middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,13 @@ import * as express from "express"
 import * as bodyParser from "body-parser"
 import { AppDataSource } from "./data-source"
 import userRouter from "./router/userRouter"
+import { errorHandler } from "./middleware/ErrorMiddleware";
 const { PORT = 3000 } = process.env;
 
 AppDataSource.initialize().then(async () => {
     const app = express()
     app.use(bodyParser.json())
+    app.use(errorHandler);
     app.use("/api", userRouter)
     app.listen(PORT)
     console.log(`Express server has started on port ${PORT}. Open http://localhost:${PORT}/users to see results`)
