4bbe9244b3beba9b97542e79ba6aa744e001c209
Thomas Lincoln <thmas5952@gmail.com>
2025-08-26 10:16:56 -0300

    feat: :sparkles: Revamp Sidebar component with updated styles and improved navigation; enhance menu and footer item rendering

--- frontend/my-invest/src/components/SideBar.vue
+++ frontend/my-invest/src/components/SideBar.vue
@@ -1,26 +1,41 @@
 <template>
   <div :class="{ 'w-64': !isCollapsed, 'w-16': isCollapsed }"
-    class="bg-gray-700 text-white flex flex-col justify-between h-screen transition-all duration-300 ease-in-out fixed">
-
-    <!-- Top part -->
+    class="bg-gray-200 text-black flex flex-col justify-between h-screen transition-all duration-300 ease-in-out fixed">
     <div class="flex flex-col flex-grow p-4">
-      <div class="mb-8 flex items-center">
-        <h1 v-if="!isCollapsed" class="text-xl font-bold">My Invest</h1>
-        <button @click="toggleSidebar" class="ml-auto p-1 rounded hover:bg-gray-800">
+      <div class="mb-8 flex items-center border-b-2 border-gray-300">
+        <h1 v-if="!isCollapsed" class="text-xl font-bold">InvestPro</h1>
+        <button @click="toggleSidebar" class="ml-auto m-2 h-10 w-10 rounded hover:bg-indigo-400 hover:text-white">
           <span class="material-symbols-outlined h-6 w-6 transform transition-transform select-none"
-            :class="{ 'rotate-180': isCollapsed }">
+            :class="{ 'rotate-180': !isCollapsed }">
             chevron_right
           </span>
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+
         </button>
       </div>
+
+      <nav class="flex-grow">
+        <ul>
+          <li v-for="item in menuItems" :key="item.name" class="mb-2">
+            <router-link :to="item.path"
+              class="flex items-center p-2 rounded-md hover:bg-indigo-400 hover:text-white transition-colors"
+              :class="{ 'justify-center': isCollapsed, 'justify-start': !isCollapsed }">
+              <span class="material-symbols-outlined h-6 w-6 select-none">
+                {{ item.icon }}
+              </span>
+              <span v-if="!isCollapsed" class="ml-4">{{ item.name }}</span>
+            </router-link>
+          </li>
+        </ul>
+      </nav>
     </div>
 
-    <!-- Middle Icons -->
-    <nav class="flex-grow">
+    <div class="p-4 border-t border-gray-700">
       <ul>
-        <li v-for="item in menuItems" :key="item.name" class="mb-2">
-          <router-link :to="item.path" class="flex items-center p-4 rounded-md hover:gray-800 transition-colors"
-            :class="{ 'jusfity-center': isCollapsed, 'justify-start': !isCollapsed }">
+        <li v-for="item in footerItems" :key="item.name" class="mb-2">
+          <router-link :to="item.path"
+            class="flex items-center p-2 rounded-md hover:bg-indigo-400 hover:text-white transition-colors"
+            :class="{ 'justify-center': isCollapsed, 'justify-start': !isCollapsed }">
             <span class="material-symbols-outlined h-6 w-6 select-none">
               {{ item.icon }}
             </span>
@@ -28,22 +43,29 @@
           </router-link>
         </li>
       </ul>
-    </nav>
-
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
+
 const isCollapsed = ref(false);
 
 const toggleSidebar = () => {
   isCollapsed.value = !isCollapsed.value;
-}
+};
 
+// Dados para os itens do menu
+// Voce pode usar uma biblioteca de icones ou copiar o path SVG.
 const menuItems = [
   {
-    icon: 'person', name: 'Perfil', path: "/profile"
+    icon: 'Home', name: 'Início', path: "/"
   }
 ]
+const footerItems = [
+  { name: 'Configuracoes', path: '/configuracoes', icon: 'settings' },
+  { name: 'Perfil', path: '/perfil', icon: 'person' }
+];
+
 </script>
