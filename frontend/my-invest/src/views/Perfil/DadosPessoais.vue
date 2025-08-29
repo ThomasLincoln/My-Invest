5fcf4e8c5a9d6d46db9beb8cfed62517d6f104bc
Thomas Lincoln <thmas5952@gmail.com>
2025-08-29 15:30:07 -0300

    feat: :sparkles: Update DadosPessoais.vue with enhanced personal information display and improved layout; add new fields for phone, email, and location

--- frontend/my-invest/src/views/Perfil/DadosPessoais.vue
+++ frontend/my-invest/src/views/Perfil/DadosPessoais.vue
@@ -2,7 +2,7 @@
 
   <!-- Informações básicas -->
 
-  <div class="bg-white m-6 rounded-lg p-8 flex border-2 border-gray-500">
+  <div class="bg-white mx-24 my-12 rounded-lg p-8 flex border-2 border-gray-500">
     <!-- Foto -->
     <div class="flex grow">
       <div class="relative">
@@ -36,53 +36,103 @@
   </div>
 
   <!-- Informações pessoais -->
-  <div class="bg-white m-6 rounded-lg p-8 border-2 border-gray-500 text-black">
-    <div class="flex align-center">
-      <span class="material-symbols-outlined mr-2">
-        person
-      </span>
-      <h1 class="font-bold text-xl">
-        Informações pessoais
-      </h1>
-    </div>
-    <p class="text-gray-500 text-sm">Gerencie suas informações básicas de perfil</p>
-    <div class="flex mt-8">
-      <div class="grow">
-        <h2>Nome Completo</h2>
-        <p>João da Silva Tannus</p>
-        <h2>Telefone</h2>
-        <div class="flex">
-          <span class="material-symbols-outlined">
-            phone_enabled
-          </span>
-          (11) 99999-9999
+  <div class="bg-white bg-white mx-24 my-12  rounded-lg p-8 border-2 border-gray-500 text-black">
+    <div class="border-b-2 pb-6 border-gray-200">
+      <div class="flex align-center gap-2">
+        <span class="material-symbols-outlined">
+          person
+        </span>
+        <h1 class="font-bold text-xl">
+          Informações pessoais
+        </h1>
+      </div>
+      <p class="text-gray-500 text-sm">Gerencie suas informações básicas de perfil</p>
+      <div class="flex flex-row gap-[40vw] mt-8">
+        <div>
+          <div class="mb-4">
+            <h2 class="text-gray-500 text-sm mb-2">Nome Completo</h2>
+            <p class="text-lg">João da Silva Tannus</p>
+          </div>
+          <div class="mb-4">
+            <h2 class="text-gray-500 text-sm mb-2">Telefone</h2>
+            <div class="flex gap-2">
+              <span class="material-symbols-outlined">
+                phone_enabled
+              </span>
+              <p class="text-lg">
+                (11) 99999-9999
+              </p>
+            </div>
+          </div>
+          <h2 class="text-gray-500 text-sm mb-2">Data de nascimento</h2>
+          <div class="flex gap-2">
+            <span class="material-symbols-outlined">
+              calendar_month
+            </span>
+            <p class="text-lg">
+              14/03/1985
+            </p>
+          </div>
         </div>
-        <h2>Data de nascimento</h2>
-        <div class="flex">
-          <span class="material-symbols-outlined">
-            calendar_month
-          </span>
-          14/03/1985
+        <div class="">
+          <div class="mb-4">
+            <h2 class="text-gray-500 text-sm mb-2">Email</h2>
+            <div class="flex gap-2">
+              <span class="material-symbols-outlined">
+                mail
+              </span>
+              <p class="text-lg">
+                joão.silva@email.com
+              </p>
+            </div>
+          </div>
+          <h2 class="text-gray-500 text-sm mb-2">Localização</h2>
+          <div class="flex gap-2">
+            <span class="material-symbols-outlined">
+              location_on
+            </span>
+            <p class="text-lg">
+              Taubaté, SP
+            </p>
+          </div>
         </div>
       </div>
+    </div>
+
+    <h2 class="text-gray-500 text-sm mt-4">Sobre você</h2>
+    <p class="text-lg">
+      Investidor de caixas de CS:go
+    </p>
+
+  </div>
+
+  <div class="bg-white bg-white mx-24 my-12  rounded-lg p-8 border-2 border-gray-500 text-black">
+    <div class="flex gap-2">
+      <span class="material-symbols-outlined">
+        shield
+      </span>
+      <h1 class="font-bold text-xl">
+        Perfil de Investimento
+      </h1>
+    </div>
+    <p class="text-gray-500 text-sm">Suas preferências e objetivos de investimento</p>
+    <div class="flex mt-8 flex-row gap-[40vw]">
       <div>
-        <h2>Email</h2>
-        <div class="flex">
-          <span class="material-symbols-outlined">
-            mail
-          </span>
-          joão.silva@email.com
-        </div>
-        <h2>Localização</h2>
-        <div class="flex">
-          <span class="material-symbols-outlined">
-            location_on
-          </span>
-          São Paulo, SP
-        </div>
+        <h2 class="text-gray-500 text-sm mb-2">Perfil de Risco</h2>
+        <span
+          class="inline-flex items-center rounded-md bg-red-400/10 px-2 py-1 text-xs font-medium text-red-500 inset-ring inset-ring-red-400/20">Investidor
+          de Risco</span>
+      </div>
+      <div>
+        <h2 class="text-gray-500 text-sm mb-2">Objetivo de Investimento</h2>
+        <p class="text-lg">
+          Comprar a Riot Games
+        </p>
+
       </div>
     </div>
   </div>
+
 </template>
 <script setup="ts">
 </script>
